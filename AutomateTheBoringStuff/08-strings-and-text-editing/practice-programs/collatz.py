c153140a20521ea08d52d10a84ed9abad47055dc
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-13 14:48:47 -0400

    Added Input Validation to "Collatz.py"
    -Added input validation to the "Collatz.py"
    practice program.

--- AutomateTheBoringStuff/08-strings-and-text-editing/practice-programs/collatz.py
+++ AutomateTheBoringStuff/08-strings-and-text-editing/practice-programs/collatz.py
@@ -13,9 +13,14 @@ def collatz(number):
 if __name__ == "__main__":
     
     collatzResult = 0
-    numInput = int(input("Please enter a number: "))
-    # Process user inputted number and convert it to an int.
     
+    while True:
+        try:
+            numInput = int(input("Please enter a number: "))
+            # Process user inputted number and convert it to an int.
+            break
+        except ValueError:
+            print("Non-integer detected, please enter an integer value.")
     while collatzResult != 1:
         numInput = collatz(numInput)
         collatzResult = numInput
